faae77325bb378eb51a8e922b86051666a11f38c
Rob <Rob@HobbyistSoftware.com>
2015-09-22 23:06:19 +0100

    more better documentation

--- RCBRightClick/RCBRightClick/RCBRightClick.h
+++ RCBRightClick/RCBRightClick/RCBRightClick.h
@@ -9,28 +9,44 @@
 #import <Foundation/Foundation.h>
 #import <Cocoa/Cocoa.h>
 
+
+/**
+ `RCBRightClick` makes it easy for you to register a right-click action through Right Click Booster
+ */
 @interface RCBRightClick : NSObject
+
+
+///------------------------------------------------
+/// @name Registering your extension
+///------------------------------------------------
+
+/**
+ Simple convenience registration method.
+ If Right Click Booster is not installed, the user will be prompted to download it from the App Store.
+ @param name Display name for the extension
+ @param scheme Scheme which will be called by RCB
+*/
++(BOOL)registerExtensionWithName:(NSString* )name scheme:(NSString* )scheme;
+
+/**
+ Full registration method.
+ If Right Click Booster is not installed, the user will be prompted to download it from the App Store.
+ @param name Display name for the extension
+ @param scheme Scheme which will be called by RCB
+ @param filetypes (Optional) case-insensitive list of filetypes which can be handled. Use 'Directory' to support directories. Note that RCB will display and act if any of the selected URLs match this list, so you need to check the returned URLs yourself
+ @param image (Optional) Image to display next to the command in the menu (30*30px)
+ @param requestCallback (Optional) RCB honours the x-callback-url scheme http://x-callback-url.com/specifications/ this parameter provides the scheme registered by your app which should receive callbacks. See the code for comments on the default callback implementation.
+*/
++(BOOL)registerExtensionWithName:(NSString* )name scheme:(NSString* )scheme filetypes:(NSArray* )fileExtensions image:(NSImage* )image callbackScheme:(NSString* )callbackScheme;
+
+
+///------------------------------------------------
+/// @name Convenience methods
+///------------------------------------------------
 
 /** RCB only works for OSX 10.10 or higher. However, it is safe to call the register... commands in lower versions. They simply no-op.
  */
 +(BOOL)systemCanHandleRCB;
-
-/**
- * Simple convenience registration method.
- * @param name Display name for the extension
- * @param scheme Scheme which will be called by RCB
- */
-+(BOOL)registerExtensionWithName:(NSString* )name scheme:(NSString* )scheme;
-
-/** 
-* Full registration method.
-* @param name Display name for the extension
-* @param scheme Scheme which will be called by RCB
-* @param filetypes (Optional) case-insensitive list of filetypes which can be handled. Use 'Directory' to support directories. Note that RCB will display and act if any of the selected URLs match this list, so you need to check the returned URLs yourself
-* @param image (Optional) Image to display next to the command in the menu (30*30px)
-* @param requestCallback (Optional) RCB honours the x-callback-url scheme http://x-callback-url.com/specifications/ this parameter provides the scheme registered by your app which should receive callbacks. See the code for comments on the default callback implementation.
-*/
-+(BOOL)registerExtensionWithName:(NSString* )name scheme:(NSString* )scheme filetypes:(NSArray* )fileExtensions image:(NSImage* )image callbackScheme:(NSString* )callbackScheme;
 
 /** When your app is called with your callback scheme, this function provides an easy way to extract the paths selected by the user from the URL. */
 +(NSArray* )pathsFromURL:(NSURL* )url;
